f9468a96e8ecae148f97c24cd1d96ba438ee7c5f
KEN <15kobaken@gmail.com>
2024-01-27 23:20:58 +0100

    add sequence clusterization add utils.visualizer

---
+++ clusterization/clusterize.py
@@ -0,0 +1,93 @@
+
+import pandas as pd
+import numpy as np
+import laspy
+import open3d 
+from settings import DATA_PATH
+from utils import visualizer
+
+
+#clusterize point clouds
+def open3d_cluster(pcd, 
+                   eps, #eps should be changed according to the distance to human.
+                   visualize_noise, #visualize noise points or not.
+                   window_name,
+                   time_duration, #how long window is visualized
+                   cam_params): 
+    
+    # pcd = open3d.geometry.PointCloud(pcd)
+    
+    # eps is the clustering distance set, and min_points is the minimum number of points required to form a class
+    labels = pcd.cluster_dbscan(eps, min_points=30, print_progress=True)
+    max_label = max(labels)
+    print("num_clusters", max_label+1)
+    
+    assert max_label!=-1, "cluster was not found by db_scan.try again by higher eps!!"
+    
+    # randomly build n+1 colors, and normalize
+    colors = np.random.randint(1, 255, size=(max_label + 1, 3)) / 255.
+    colors = colors[labels]             # determines point's color according to its label
+    colors[np.array(labels) < 0] = 0    # The noise configuration is black
+    
+    pcd.colors = open3d.utility.Vector3dVector(colors)  # Format conversion (because pcd.colors requires Vector3dVector format)
+    
+    # delete noise points
+    if not visualize_noise:
+        #select only clusterized points
+        pcd = pcd.select_by_index(np.where(np.array(labels) != -1)[0]) 
+    
+    
+    # close window at the time of selected time duration
+    if time_duration is not None:
+        # Visual point cloud list
+        visualizer.visualize([pcd],time_duration=time_duration,
+                            window_name=window_name,cam_params=cam_params)
+    # normal window
+    else: 
+        open3d.visualization.draw_geometries([pcd],
+                                    window_name=window_name)
+    return labels
+
+
+# fit plane and delete points on the plane
+def fit_plane(pcd, distance_threshold, visualize_plane):   
+    #pcd = open3d.io.read_point_cloud(pcd_file)
+    plane_model, inliers = pcd.segment_plane(distance_threshold,
+                                            ransac_n=3,
+                                            num_iterations=1000)
+    [a, b, c, d] = plane_model
+    print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+
+    inlier_cloud = pcd.select_by_index(inliers)
+    inlier_cloud.paint_uniform_color([1.0, 0, 0])
+    outlier_cloud = pcd.select_by_index(inliers, invert=True)
+    
+    if visualize_plane==True: #weather visualize plane fitting or not
+        open3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+    
+    return outlier_cloud
+
+#one cycle of clustrization
+def clusterize(pcd, distance_threshold=0.05, num_planes=2, visualize_plane=False,
+               eps=0.3, window_name="cluster", visualize_noise=False, 
+               time_duration=None, cam_params=None
+               ):
+    
+    #iterate fitting planes for "num_planes" times
+    for i in range(num_planes):
+        pcd = fit_plane(pcd, distance_threshold, visualize_plane=visualize_plane)
+    
+    outlier_cloud = pcd
+    labels = open3d_cluster(outlier_cloud, eps, visualize_noise, window_name, time_duration,cam_params)
+    return labels
+
+def main():
+    pcd_file = rf'{DATA_PATH}/LIVOX_Hallway_pcds/res100ms_start30s/res100ms_start30s/00100.pcd'
+    eps=0.5
+    num_planes=2 #how many times plane fitting is applied
+    
+    pcd = open3d.io.read_point_cloud(pcd_file)
+    clusterize(pcd, eps=eps)
+    
+if __name__ == "__main__":
+    main()
---
+++ utils/visualizer.py
@@ -0,0 +1,63 @@
+import time
+import open3d as o3d
+
+def visualize(pcd_list, time_duration=100, window_name="cluster", width=1920, height=1080, cam_params=None):
+    # 可視化ウィンドウを作成
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(window_name=window_name, width=width, height=height, left=50, top=50, visible=True)
+
+    if type(pcd_list)!=list:
+        # ポイントクラウドを追加
+        vis.add_geometry(pcd_list)
+    else:
+        for geometry in pcd_list:
+            vis.add_geometry(geometry)
+    
+    if cam_params is not None:
+        view_control = vis.get_view_control()
+        view_control.convert_from_pinhole_camera_parameters(cam_params,allow_arbitrary=True)
+    
+    # 描画
+    vis.poll_events()
+    vis.update_renderer()
+
+    # 待機
+    time.sleep(time_duration)
+
+    # ウィンドウを閉じる
+    vis.destroy_window()
+    
+'''
+How to use?
+
+cam_params = save_camera_params(pcd)
+load_camera_params(pcd, cam_params)
+'''
+
+def save_camera_params(pcd):
+    # make setting window
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(pcd)
+    view_control = vis.get_view_control()
+
+    # wait for setting the view by user
+    print("set the view point and close the window")
+    vis.run()
+
+    # save camera params
+    cam_params = view_control.convert_to_pinhole_camera_parameters()
+    vis.destroy_window()
+    return cam_params
+
+def load_camera_params(pcd, cam_params):
+    # load camera params
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(pcd)
+    view_control = vis.get_view_control()
+    view_control.convert_from_pinhole_camera_parameters(cam_params,allow_arbitrary=True)
+
+    # 再現した視点で表示
+    vis.run()
+    vis.destroy_window()
