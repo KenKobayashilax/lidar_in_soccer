e4b9fce60df5f1a1b5984453b581ceaab21b8a6f
xyuhuang <xiaoyuh98@gmail.com>
2024-02-02 20:55:12 +0800

    add bbox method

--- detection/detect.py
+++ detection/detect.py
@@ -29,6 +29,52 @@ def compute_nearest_cluster(pcd, labels, center_prev):
     
     return nearest_cluster_id
 
+
+# compute bbox of clusters
+def compute_bbox(pcd, labels):
+    filtered_labels = []
+    
+    # convert to array
+    points = np.asarray(pcd.points)
+    
+    #get label values
+    unique_labels = np.unique(labels)
+    
+    bboxes = []
+    
+    for label in unique_labels:
+        # extract the points belonging to the current clustering
+        cluster_points = points[labels == label]
+
+        # compute bbox
+        if len(cluster_points) > 0:
+            bbox_min = np.min(cluster_points, axis=0)
+            bbox_max = np.max(cluster_points, axis=0)
+        
+            print(f"Cluster {label} Bounding Box:")
+            
+            bboxes.append({
+                'label': label,
+                'min': bbox_min,
+                'max': bbox_max
+            })
+    
+    for bbox in bboxes:
+        # compute height, length and width of bbox
+        height = bbox['max'][2] - bbox['min'][2]
+        length = bbox['max'][0] - bbox['min'][0]
+        width = bbox['max'][1] - bbox['min'][1]
+
+        # check if bbox seems like a human
+        if 1.5 <= height <= 2.2 and 0.3 <= length <= 2 and 0.3 <= width <= 2:
+            filtered_labels.append(bbox['label'])
+    
+    print('filtered_labels:', filtered_labels)  
+    return filtered_labels
+
+
+
+
 #change color of only selected cluster
 def change_cluster_color(pcd, labels, cluster_id, color=[1.0, 0.0, 0.0]):
     colors = np.asarray(pcd.colors)
